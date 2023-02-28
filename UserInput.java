2f20e376f3079be11b20b869e075c281574044c2
Takeru9016 <youthfox046@gmail.com>
2023-02-28 16:44:27 +0530

    Updated UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -5,5 +5,9 @@ public static void main (String args[])
 {
 System.out.println("My first name is "+" "+args[0]);
 System.out.println("My last name is "+" "+args[1]);
+System.out.println("My first name is "+" "+args[0]);
+System.out.println("My last name is "+" "+args[1]);
+System.out.println("My first name is "+" "+args[0]);
+System.out.println("My last name is "+" "+args[1]);
 }
 }
